ee25cd1c126f3e7c710bfdc5f49f871741186fff
Anndersonn <slobodjanik1997@gmail.com>
2021-03-08 20:46:35 +0100

    axios and prop types

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import './scss/app.scss'
 import { Header } from './components';
 import { Home, Cart } from './pages';
-import { Route } from 'react-router-dom'
+import { Route } from 'react-router-dom';
+
+
 
 function App() {
+  const [pizzas, setPizzas] = useState([])
+  useEffect(() => {
+    axios.get('http://localhost:3000/db.json').then(({ data }) => setPizzas(data.pizzas))
+
+  }, []);
+
   return (
     <div>
       <div className="wrapper">
         <Header />
         <div className="content">
-          <Route exact path='/' component={Home} />
+          <Route exact path='/' render={() => <Home items={pizzas} />} />
           <Route exact path='/cart' component={Cart} />
         </div>
       </div>
