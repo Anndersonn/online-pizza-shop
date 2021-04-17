5735f575db5ab39fe81ff356f45943296a5f9e2b
Anndersonn <slobodjanik1997@gmail.com>
2021-04-17 15:51:01 +0200

    removing pizzas from the cart

--- src/redux/actions/cart.js
+++ src/redux/actions/cart.js
@@ -2,3 +2,12 @@ export const addPizzaToCart = (pizzaObj) => ({
     type: 'ADD_PIZZA_CART',
     payload: pizzaObj
 })
+
+export const clearCart = () => ({
+    type: 'CLEAR_CART',
+})
+
+export const removeItemCart = (id) => ({
+    type: 'REMOVE_ITEM_CART',
+    payload: id
+})
--- src/redux/reducers/cart.js
+++ src/redux/reducers/cart.js
@@ -4,21 +4,47 @@ const initialState = {
     totalCount: 0
 }
 
+const getTotalPrice = arr => arr.reduce((sum, obj) => obj.price + sum, 0)
+
 const cart = (state = initialState, action) => {
     switch (action.type) {
         case 'ADD_PIZZA_CART': {
+            const currentPizzaItems = !state.items[action.payload.id] ? [action.payload] : [...state.items[action.payload.id].items, action.payload]
+
             const newItems = {
                 ...state.items,
-                [action.payload.id]: !state.items[action.payload.id] ? [action.payload] : [...state.items[action.payload.id], action.payload]
-            }
+                [action.payload.id]: {
+                    items: currentPizzaItems,
+                    totalPrice: getTotalPrice(currentPizzaItems)
+                }
 
-            const allPizzas = [].concat.apply([], Object.values(newItems)) // concatination of all added pizzas to one array.
-            const totalPrice = allPizzas.reduce((sum, obj) => obj.price + sum, 0)
+            }
+            const items = Object.values(newItems).map(obj => obj.items)
+            const allPizzas = [].concat.apply([], items)
+            const totalPrice = getTotalPrice(allPizzas)
             return {
                 ...state,
                 items: newItems,
                 totalCount: allPizzas.length,
                 totalPrice
+            }
+
+        }
+        case 'REMOVE_ITEM_CART': {
+            const newItems = {
+                ...state.items
+            }
+            delete newItems[action.payload];
+            return {
+                ...state,
+                items: newItems
+            }
+        }
+        case 'CLEAR_CART': {
+            return {
+                totalPrice: 0,
+                totalCount: 0,
+                items: {},
             }
         }
         default: return state;
